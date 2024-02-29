a8e17eb1eb28f88bc1294d8eea9e4ee2e872565d
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2024-02-29 19:26:04 +0000

    Solution.

--- src/controller.ts
+++ src/controller.ts
@@ -1,26 +1,151 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { database } from "./model";
+import { party } from "./model";
 
-/**
- * TODO: Copy the route handling logic from the previous exercise
- * into these functions. You will need to use the party array from
- * the model.ts file to handle the requests.
- */
+export const getHome = (req: IncomingMessage, res: ServerResponse) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(
+		JSON.stringify(
+			{
+				message: "Hello from the Pokemon Server!",
+			},
+			null,
+			2,
+		),
+	);
+};
 
-// GET /
-export const getHome = (req: IncomingMessage, res: ServerResponse) => {};
+export const getAllPokemon = (req: IncomingMessage, res: ServerResponse) => {
+	const url = new URL(req.url!, `http://${req.headers.host}`); // Use URL parsing
+	const queryParams = url.searchParams;
+	const typeFilter = queryParams.get("type");
+	const sortBy = queryParams.get("sortBy");
 
-// GET /pokemon
-export const getAllPokemon = (req: IncomingMessage, res: ServerResponse) => {};
+	// Apply basic filtering using 'if' if we have a `typeFilter`:
+	if (typeFilter) {
+		const filteredParty = party.filter(
+			(pokemon) => pokemon.type === typeFilter,
+		);
+		res.end(JSON.stringify(filteredParty, null, 2));
+		return;
+	}
 
-// GET /pokemon/:id
-export const getOnePokemon = (req: IncomingMessage, res: ServerResponse) => {};
+	if (sortBy === "name") {
+		const sortedParty = [...party].sort((a, b) =>
+			a.name.localeCompare(b.name),
+		);
+		res.end(JSON.stringify(sortedParty, null, 2));
+		return;
+	} // ... (you can handle sorting by other properties if needed)
 
-// POST /pokemon
-export const createPokemon = (req: IncomingMessage, res: ServerResponse) => {};
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(
+		JSON.stringify({ message: "All Pokemon", payload: party }, null, 2),
+	);
+};
 
-// PUT /pokemon/:id
-export const updatePokemon = (req: IncomingMessage, res: ServerResponse) => {};
+export const getOnePokemon = (req: IncomingMessage, res: ServerResponse) => {
+	const id = Number(req.url?.split("/")[2]);
+	const foundPokemon = party.find((pokemon) => pokemon.id === id);
 
-// DELETE /pokemon/:id
-export const deletePokemon = (req: IncomingMessage, res: ServerResponse) => {};
+	if (foundPokemon) {
+		res.statusCode = 200;
+		res.setHeader("Content-Type", "application/json");
+		res.end(
+			JSON.stringify(
+				{
+					message: "Pokemon found",
+					payload: foundPokemon,
+				},
+				null,
+				2,
+			),
+		);
+	} else {
+		res.statusCode = 404;
+		res.end(JSON.stringify({ message: "Pokemon not found" }, null, 2));
+	}
+};
+
+export const createPokemon = (req: IncomingMessage, res: ServerResponse) => {
+	let body = "";
+
+	req.on("data", (chunk) => {
+		body += chunk.toString();
+	});
+
+	req.on("end", () => {
+		const newPokemon = JSON.parse(body);
+		newPokemon.id = party.length + 1; // Simple ID assignment
+		party.push(newPokemon);
+
+		res.statusCode = 201; // 'Created'
+		res.setHeader("Content-Type", "application/json");
+		res.end(
+			JSON.stringify(
+				{
+					message: "Pokemon created!",
+					payload: newPokemon,
+				},
+				null,
+				2,
+			),
+		);
+	});
+};
+
+export const updatePokemon = (req: IncomingMessage, res: ServerResponse) => {
+	const id = Number(req.url?.split("/")[2]);
+	let body = "";
+
+	req.on("data", (chunk) => {
+		body += chunk.toString();
+	});
+
+	req.on("end", () => {
+		const updatedPokemon = JSON.parse(body);
+		const foundIndex = party.findIndex((p) => p.id === id);
+		if (foundIndex !== -1) {
+			party[foundIndex] = {
+				...party[foundIndex],
+				...updatedPokemon,
+			};
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			res.end(
+				JSON.stringify(
+					{
+						message: "Pokemon updated!",
+						payload: party[foundIndex],
+					},
+					null,
+					2,
+				),
+			);
+		} else {
+			res.statusCode = 404;
+			res.end(JSON.stringify({ message: "Pokemon not found" }, null, 2));
+		}
+	});
+};
+
+export const deletePokemon = (req: IncomingMessage, res: ServerResponse) => {
+	const id = Number(req.url?.split("/")[2]);
+	const foundIndex = party.findIndex((p) => p.id === id);
+
+	if (foundIndex !== -1) {
+		const deletedPokemon = party.splice(foundIndex, 1); // Remove the Pokemon
+		res.statusCode = 200;
+		res.end(
+			JSON.stringify(
+				{ message: "Pokemon deleted", payload: deletedPokemon },
+				null,
+				2,
+			),
+		);
+	} else {
+		res.statusCode = 404;
+		res.end(JSON.stringify({ message: "Pokemon not found" }, null, 2));
+	}
+};
--- src/server.ts
+++ src/server.ts
@@ -10,34 +10,16 @@ import {
 const hostname = "127.0.0.1";
 const port = 3000;
 
-/**
- * A blueprint defining what a route handling function looks like.
- * A route handler takes a request and response object.
- * While it does not return anything, it is expected to send a response
- * back to the client using the response object.
- */
 interface RouteHandler {
 	(req: IncomingMessage, res: ServerResponse): void;
 }
 
-/**
- * A blueprint defining what the routes object looks like.
- * The routes object is a dictionary of HTTP methods.
- * Each HTTP method is a dictionary of routes.
- * Each route is a string that maps to a route handling function.
- * @see https://www.typescriptlang.org/docs/handbook/advanced-types.html#index-types-and-index-signatures
- */
 interface Routes {
 	[method: string]: {
 		[path: string]: RouteHandler;
 	};
 }
 
-/**
- * The routes object is a dictionary of HTTP methods.
- * Each HTTP method is a dictionary of routes.
- * Each route is a string that maps to a route handling function.
- */
 const routes: Routes = {
 	GET: {},
 	POST: {},
@@ -45,20 +27,12 @@ const routes: Routes = {
 	DELETE: {},
 };
 
-/**
- * A function that handles all incoming requests.
- * It logs the request method and URL to the console.
- * It then checks if the request method and URL match a route handler.
- * If a route handler is found, it calls the route handler.
- * If a route handler is not found, it sends a 404 response.
- * @param req The request object.
- * @param res The response object.
- */
 const handleRequest = (req: IncomingMessage, res: ServerResponse) => {
 	console.log(`${req.method} ${req.url}`);
 
-	// TODO: Determine the route handler based on the request method and URL.
-	const handler = routes["GET"]["/"];
+	let url = req.url?.startsWith("/pokemon/") ? "/pokemon/:id" : req.url;
+	url = url?.split("?")[0]; // Get rid of query parameters.
+	const handler = routes[req.method ?? "GET"][url ?? "/"];
 
 	if (handler) {
 		handler(req, res);
@@ -68,16 +42,12 @@ const handleRequest = (req: IncomingMessage, res: ServerResponse) => {
 	}
 };
 
-/**
- * Route registration is the process of adding a route to the routes object.
- * Populate the routes object by placing function references,
- * e.g., the `getAllPokemon` function you made, into these slots to establish paths.
- */
 routes.GET["/"] = getHome;
+routes.GET["/pokemon"] = getAllPokemon;
 routes.GET["/pokemon/:id"] = getOnePokemon;
 routes.POST["/pokemon"] = createPokemon;
-
-// TODO: Add the remaining routes to the routes object.
+routes.PUT["/pokemon/:id"] = updatePokemon;
+routes.DELETE["/pokemon/:id"] = deletePokemon;
 
 const server = http.createServer(handleRequest);
 
